b5fcb067ac3de31d4d6ca50bb98d88659120d443
Steven Prins <git@stevenprins.com>
2018-12-20 22:27:42 +0100

    Improve 404

--- app.go
+++ app.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -29,10 +30,26 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	if urlPath == "/index.html" {
 		log.Print("Request index: " + path)
 
-		f, _ := os.Open(path)
+		f, err := os.Open(path)
+		if err != nil {
+			errorHandler(w, r, http.StatusNotFound)
+			return
+		}
 		http.ServeContent(w, r, path, time.Now(), f)
+	}
+
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		errorHandler(w, r, http.StatusNotFound)
+		return
 	}
 
 	log.Print("Request: " + path)
 	http.ServeFile(w, r, path)
+}
+
+func errorHandler(w http.ResponseWriter, r *http.Request, status int) {
+	w.WriteHeader(status)
+	if status == http.StatusNotFound {
+		fmt.Fprint(w, "")
+	}
 }
